cab35c7d8ae5cb53e15803e3f84acdbce8fb9159
islammd39e <islammdrahat308@gmail.comm>
2022-10-11 20:37:03 +0600

    customize red me file test

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
-# Getting Started with Create React App
+
+<!--
+https://steady-muffin-6185a2.netlify.app 
+ -->
+
+## This is my live web site link : https://steady-muffin-6185a2.netlify.app 
+
+
+
+
+
+
+
+<!-- # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
@@ -67,5 +80,5 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 
 ### `npm run build` fails to minify
 
-This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify) -->
 
--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -11,7 +11,6 @@ const Blog = () => {
             <p>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.</p>
             <h5>what is useRef in react?</h5>
             <p>useRef(initialValue) is a built-in React hook that accepts one argument as the initial value and returns a reference (aka ref). A reference is an object having a special property current. When the button is clicked, handle function is invoked and the reference value is incremented: countRef.current++. The reference value is logged to the console.Updating the reference value countRef.current++ doesn't trigger component re-rendering. This is demonstrated by the fact that 'I rendered!' is logged to the console just once, at initial rendering, and no re-rendering happens when the reference is updated.</p>
-            
             </blockquote>
         </Container>
     );
